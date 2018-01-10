301910d20253426947662351ecb11f47266e1474
vivekhs <viveksinha461@gmail.com>
2018-01-10 11:04:22 +0530

    initial commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,7 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+export const APP_ROUTES: Routes=[
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+declare var jQuery: any;
+
+@Component({
+    selector: 'home-page',
+    templateUrl: 'home.component.html'
+})
+
+export class HomeComponent implements OnInit {
+     ngOnInit(){
+        
+     }
+}
